176a1f665e8cc5a614fdaa5dac1d337a1fd99aeb
kais ajili <ajilikais222@gmail.com>
2024-12-01 14:06:50 +0100

    Initial commit - Projet Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProduitComponent } from './produit/produit.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StockComponent } from './stock/stock.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+      path: '', 
+      redirectTo: 'Home', 
+      pathMatch: 'full' 
+    },
+    { 
+      path: 'Home',
+      component: HomeComponent,
+      children: [
+        {
+          path: '',
+          redirectTo: 'Dashboard', // Redirige vers "Dashboard" par défaut
+          pathMatch: 'full'
+        },
+        {
+          path: 'Dashboard', // Sous-route pour "Dashboard"
+          component: DashboardComponent, 
+          pathMatch: 'full'
+        },
+        {
+          path: 'Produit', // Sous-route pour "Produit"
+          component: ProduitComponent,
+        },
+        {
+          path: 'Stock', // Sous-route pour "Stock"
+          component: StockComponent,
+        }
+      ]
+    },
+    { path: '**', redirectTo: 'Home' } // Redirection pour les chemins non définis
+  ];
+  
